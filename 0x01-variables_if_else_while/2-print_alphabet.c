828c4429d21233acccfea171bb3b9abf50e17f7d
omegamudzviti_engineering <omegamudzviti@gmail.com>
2022-07-01 13:51:29 -0700

    task 3 to 7 complited

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
- *
- * Return: Always 0.
- */
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
- char letter;
+char letter;
 
- for (letter = 'a'; letter <= 'z'; letter++)
-  putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
 
- putchar('\n');
+putchar('\n');
 
- return (0);
+return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0.
- *
- * Return: Always 0.
- */
+* main - Prints all single digit numbers of base 10 starting from 0.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
- int num;
+int num;
 
- for (num = 0; num < 10; num++)
-  printf("%d", num);
+for (num = 0; num < 10; num++)
+printf("%d", num);
 
- printf("\n");
+printf("\n");
 
- return (0);
+return (0);
 }
