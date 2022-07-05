01a69a94212a1a1ac84b76eb15a04a88adfb627d
omegamudzviti_engineering <omegamudzviti@gmail.com>
2022-07-05 09:12:24 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
-int main(){
+#include <main.h>
 
+/** prototype of the function _putchar is included in your header file called main.h
+ *  return ; 0 **/
 
-printf("_putchar\n");
-
-return 0;
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
